c6c09c7a4ff0258de834ab5880b190064bdfbf0e
David Martin <dmartin5150@icloud.com>
2023-11-23 15:35:08 -0600

    adding jest config

--- jest.config.js
+++ jest.config.js
@@ -11,26 +11,8 @@
 // };
 
 module.exports = {
-  preset: "jest-expo",
-  transform: {
-    "^.+\\.(js|jsx|ts|tsx)$": "ts-jest",
-  },
-  globals: {
-    "ts-jest": {
-      transformerConfig: {
-        transformIgnorePatterns: [
-          "<rootDir>/node_modules/(react-clone-referenced-element|@react-native-community|react-navigation|@react-navigation/.*|@unimodules/.*|native-base|react-native-code-push)",
-          "jest-runner",
-        ],
-      },
-    },
-  },
+
   testPathIgnorePatterns: [
-    "<rootDir>/node_modules/",
-    "<rootDir>/.maestro/",
-    "@react-native",
+    "<rootDir>/node_modules/*",
   ],
-  testEnvironment: "react-native",
-  setupFiles: ["<rootDir>/test/setup.ts"],
-  setupFilesAfterEnv: ["<rootDir>/jest-setup-after-env.js"],
 };
