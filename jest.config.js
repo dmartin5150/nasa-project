85b2810cc6deb310589908bd97ab0d473d689477
David Martin <dmartin5150@icloud.com>
2023-11-23 15:13:08 -0600

    adding jest config

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,36 @@
+// module.exports = {
+//   testMatch: ['**/+(*.)+(spec|test).+(ts|js)?(x)'],
+//   transform: {
+//     '^.+\\.(ts|js|html)$': 'jest-preset-angular/preprocessor.js'
+//   },
+//   // transformIgnorePatterns: ['node_modules/(?!lodash-es/*|@angular/common/locales/*)'],
+//   transformIgnorePatterns: ["node_modules/(?!@ngrx|(?!deck.gl)|ng-dynamic)"],
+//   resolver: '@nrwl/builders/plugins/jest/resolver',
+//   moduleFileExtensions: ['ts', 'js', 'html'],
+//   coverageReporters: ['json', 'html', 'cobertura', 'text-summary']
+// };
+
 module.exports = {
-  testMatch: ['**/+(*.)+(spec|test).+(ts|js)?(x)'],
+  preset: "jest-expo",
   transform: {
-    '^.+\\.(ts|js|html)$': 'jest-preset-angular/preprocessor.js'
+    "^.+\\.(js|jsx|ts|tsx)$": "ts-jest",
   },
-  // transformIgnorePatterns: ['node_modules/(?!lodash-es/*|@angular/common/locales/*)'],
-  transformIgnorePatterns: ["node_modules/(?!@ngrx|(?!deck.gl)|ng-dynamic)"],
-  resolver: '@nrwl/builders/plugins/jest/resolver',
-  moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageReporters: ['json', 'html', 'cobertura', 'text-summary']
+  globals: {
+    "ts-jest": {
+      transformerConfig: {
+        transformIgnorePatterns: [
+          "<rootDir>/node_modules/(react-clone-referenced-element|@react-native-community|react-navigation|@react-navigation/.*|@unimodules/.*|native-base|react-native-code-push)",
+          "jest-runner",
+        ],
+      },
+    },
+  },
+  testPathIgnorePatterns: [
+    "<rootDir>/node_modules/",
+    "<rootDir>/.maestro/",
+    "@react-native",
+  ],
+  testEnvironment: "react-native",
+  setupFiles: ["<rootDir>/test/setup.ts"],
+  setupFilesAfterEnv: ["<rootDir>/jest-setup-after-env.js"],
 };
