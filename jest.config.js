d71a7ea119c99b5f2cc124a92ba3e00803f88225
David Martin <dmartin5150@icloud.com>
2023-11-23 14:53:02 -0600

    adding jest config

--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,7 @@ module.exports = {
     '^.+\\.(ts|js|html)$': 'jest-preset-angular/preprocessor.js'
   },
   transformIgnorePatterns: ['node_modules/(?!lodash-es/*|@angular/common/locales/*)'],
+  transformIgnorePatterns: ["node_modules/(?!@ngrx|(?!deck.gl)|ng-dynamic)"],
   resolver: '@nrwl/builders/plugins/jest/resolver',
   moduleFileExtensions: ['ts', 'js', 'html'],
   coverageReporters: ['json', 'html', 'cobertura', 'text-summary']
